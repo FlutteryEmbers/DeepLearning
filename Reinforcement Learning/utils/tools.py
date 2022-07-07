52e03722236607558d25e8ff44624a1b25be0310
shili.wu <shili.wu@hotmail.com>
2022-07-07 08:45:58 -0500

    structure_change

--- Reinforcement Learning/utils/tools.py
+++ Reinforcement Learning/utils/tools.py
@@ -1,21 +1,48 @@
+import math
+
+import numpy as np
+
 import torch
 
-def flat_params(model):
-    '''
-        把模型里边的参数展平
-    '''
+
+def normal_entropy(std):
+    var = std.pow(2)
+    entropy = 0.5 + 0.5 * torch.log(2 * var * math.pi)
+    return entropy.sum(1, keepdim=True)
+
+
+def normal_log_density(x, mean, log_std, std):
+    var = std.pow(2)
+    log_density = -(x - mean).pow(2) / (
+        2 * var) - 0.5 * math.log(2 * math.pi) - log_std
+    return log_density.sum(1, keepdim=True)
+
+
+def get_flat_params_from(model):
     params = []
     for param in model.parameters():
         params.append(param.data.view(-1))
-    params_flatten = torch.cat(params)
-    return params_flatten
+
+    flat_params = torch.cat(params)
+    return flat_params
 
 
-def update_model(model, new_params):
-    index = 0
-    for params in model.parameters():
-        params_length = len(params.view(-1))
-        new_param = new_params[index: index + params_length]
-        new_param = new_param.view(params.size())
-        params.data.copy_(new_param)
-        index += params_length
+def set_flat_params_to(model, flat_params):
+    prev_ind = 0
+    for param in model.parameters():
+        flat_size = int(np.prod(list(param.size())))
+        param.data.copy_(
+            flat_params[prev_ind:prev_ind + flat_size].view(param.size()))
+        prev_ind += flat_size
+
+
+def get_flat_grad_from(net, grad_grad=False):
+    grads = []
+    for param in net.parameters():
+        if grad_grad:
+            grads.append(param.grad.grad.view(-1))
+        else:
+            grads.append(param.grad.view(-1))
+
+    flat_grad = torch.cat(grads)
+    return flat_grad
